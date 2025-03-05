8a4bb37e5e6b68d113bd1116eb14e83e8d9581c3
mehedi <mehedi25122003@gmail.com>
2025-03-05 17:40:35 +0600

    header almost done

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaBitcoin } from "react-icons/fa";
+
+const Header = () => {
+    return (
+        <div className='flex justify-between pt-2'>
+            <div>
+                <img src="/images/logo.png" alt="" />
+            </div>
+            <div className='flex gap-5 items-center text-gray-600'>
+                <p>Home</p>
+                <p>Fixture</p>
+                <p>Teams</p>
+                <p>Schedules</p>
+                <button className='btn border-2 p-2 rounded-xl inline-flex items-center gap-1 font-bold text-2xl'>
+                    <span>0</span> Coin
+                    <FaBitcoin></FaBitcoin>
+                    
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
