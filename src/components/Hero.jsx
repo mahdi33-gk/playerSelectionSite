cf73216481c08a4378db1f8104cdf689555e32a9
mehedi <mehedi25122003@gmail.com>
2025-03-05 19:12:07 +0600

    miss daisyUI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div>
             
-            <img className='w-full object-cover rounded-xl' src="/images/bg-shadow.png" alt="" />
+            <img className='w-full relative object-cover rounded-xl' src="/images/bg-shadow.png" alt="" />
             <img className='absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src="/images/banner-main.png" alt="" />
         </div>
     );
